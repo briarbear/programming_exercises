2241f91506a91cfd6bee312ef3cf301a4c54af44
XbearE480 <xiongzp08@163.com>
2018-05-04 22:25:23 +0800

    完成回文串dp实现

--- src/main/java/leetcode/ch0/S5.java
+++ src/main/java/leetcode/ch0/S5.java
@@ -16,7 +16,7 @@ public class S5 {
      * @return
      */
     public String longestPalindrome1(String s) {
-
+        //利用一种中心点游标,中心点游标每次移动0.5距离，然后向两边扩展，判断是否为回文字串
         char[] chars = s.toCharArray();
         int len = chars.length;
         int max = 1,start= 0,end = 0;
@@ -54,6 +54,7 @@ public class S5 {
 
     /**
      * 尝试动态规划解决
+     * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming
      * @param s
      * @return
      */
@@ -61,14 +62,46 @@ public class S5 {
 
         char[] chars  = s.toCharArray();
         int len = chars.length;
+        String res = null;
         boolean[][] dp = new boolean[len][len];   //dp[i][j]表示s(i....j)的字串 是否为回文串
 
-        for (int i = len-1;i>=0;i--){
+        for (int i = len-1;i>=0;i--){  //从最右端开始
             for (int j = i; j < len ; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) &&(j-i<3 || dp[i+1][j-1]);
 
+                //判断是否为最长
+                if (dp[i][j] && (res == null || j-i+1 > res.length()))
+                    res = s.substring(i,j+1);
             }
         }
 
+        return res;
 
     }
+
+    public String longestPalindrome3(String s) {
+        //尝试从左端开始
+        String res = null;
+        int len = s.length();
+        //dp二维数组 dp[i][j]表示s中从i到j的字串是否为回文字串
+        boolean[][] dp = new boolean[len][len];
+
+        for (int j = 0; j < len; j++) {
+            for (int i = j; i >= 0; i--) {
+                //如果两端的字母相等，且长度<=3 或者去掉两端的也符合回文串
+                dp[i][j] = s.charAt(i)==s.charAt(j) && (j-i+1 <=3 || dp[i+1][j-1]);
+
+                //判断是否为最长
+                if (dp[i][j] && (res == null || j-i+1 > res.length()))
+                    res = s.substring(i,j+1);
+            }
+
+
+        }
+
+        return res;
+
+    }
+
+
 }
--- src/test/java/leetcode/ch0/S5Test.java
+++ src/test/java/leetcode/ch0/S5Test.java
@@ -10,7 +10,13 @@ public class S5Test {
     @Test
     public void longestPalindrome1() {
         S5 test = new S5();
-        ;
         Assert.assertEquals("bab",test.longestPalindrome1("babad"));
+    }
+
+    @Test
+    public void longestPalindrome2() {
+
+        S5 s5 = new S5();
+        Assert.assertEquals(5,s5.longestPalindrome3("ababa").length());
     }
 }
