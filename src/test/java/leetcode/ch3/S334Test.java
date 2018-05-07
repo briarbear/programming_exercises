b77b490653c57bef38c7a3266ebcc5fdf68ee696
XbearE480 <xiongzp08@163.com>
2018-05-07 23:39:23 +0800

    完成334的四种解法

--- src/test/java/leetcode/ch3/S334Test.java
+++ src/test/java/leetcode/ch3/S334Test.java
@@ -9,7 +9,11 @@ public class S334Test {
     @Test
     public void increasingTriplet() {
         S334 s = new S334();
-        int[] ints = new int[]{2,1,0,2,0,2,0,2,0,2,0,1};
-        System.out.println(s.increasingTriplet(ints));
+//        int[] ints = new int[]{2,1,0,2,0,2,0,2,0,2,0,1};
+//        int[] ints = new int[]{1,2,3,1,2,1};
+//        int[] ints = new int[]{4,7,2,1,5,6};
+        int[] ints = new int[]{4,7};
+//        System.out.println(s.increasingTriplet(ints));
+        System.out.println(s.increasingTriplet4(ints));
     }
 }
