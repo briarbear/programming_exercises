87dc5d0976c3f72f1bad85de1066b44d022fde62
XbearE480 <xiongzp08@163.com>
2018-05-09 22:57:46 +0800

    完成2的精简解法

--- src/test/java/leetcode/ch0/S2Test.java
+++ src/test/java/leetcode/ch0/S2Test.java
@@ -10,14 +10,18 @@ public class S2Test {
     public void addTwoNumbers() {
         ListNode l1 = new ListNode(9);
         l1.next = new ListNode(8);
-//        l1.next.next = new ListNode(9);
-//        l1.next.next.next = new ListNode(7);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(7);
 
         ListNode l2 = new ListNode(1);
-//        l2.next = new ListNode(6);
-//        l2.next.next = new ListNode(4);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
 
         S2 s = new S2();
-        s.addTwoNumbers(l1,l2);
+        ListNode res = s.addTwoNumbers2(l1,l2);
+        while (res != null){
+            System.out.print(res.val+ "->");
+            res = res.next;
+        }
     }
 }
