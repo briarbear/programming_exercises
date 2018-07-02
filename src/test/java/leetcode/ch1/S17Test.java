bdb2a7f5dd2f0c2506cad525dca3af3179f9a534
briarbear <xiongzp08@163.com>
2018-07-02 18:07:43 +0800

    update

--- src/test/java/leetcode/ch1/S17Test.java
+++ src/test/java/leetcode/ch1/S17Test.java
@@ -1,5 +1,6 @@
 package leetcode.ch1;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -13,6 +14,13 @@ public class S17Test {
         S17 s = new S17();
         String digits = "23";
         List<String> res = s.letterCombinations(digits);
+        Assert.assertEquals("xzp",System.out.printf("xzp"));
         System.out.println();
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("");
     }
 }
