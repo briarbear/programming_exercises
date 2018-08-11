42e8c19da0f544ac8146d5936f24b5124205c9d1
XbearE480 <xiongzp08@163.com>
2018-08-11 13:01:19 +0800

    网易编程题-牛牛招工作待完成

---
+++ src/main/java/company/wangyi/FindJob.java
@@ -0,0 +1,24 @@
+package company.wangyi;
+
+import java.util.Scanner;
+
+/**
+ * 牛牛找工作
+ *
+ * @author briarbear
+ * @blog http://briarbear.github.io
+ * @create 2018-08-11-12:57
+ */
+public class FindJob {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();  //小伙伴数量
+
+        }
+    }
+}
