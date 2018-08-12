eff00ab91541c80c3c9987c2e77e74e8d45810f8
briarbear <xiongzp08@163.com>
2018-08-12 09:53:30 +0800

    更新

---
+++ src/main/java/company/Ali/S0802.java
@@ -0,0 +1,31 @@
+package company.Ali;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 阿里编程题
+ *
+ * @author briarbear
+ * @blog https://briarbear.github.io
+ * @create 2018-08-02-14:57
+ */
+public class S0802 {
+
+    public static void main(String[] args) {
+
+        Map<String,String> map = new HashMap<>();
+        String key;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String[] str = sc.next().split(" ");
+
+        }
+
+
+    }
+
+
+
+}
